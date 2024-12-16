54be35f1ef48f3c0cb9b85852347e0baf3b80437
hendrik.schulze.broering <hendrik.schulze.broering@googlemail.com>
2024-12-16 17:20:40 +0100

    Initial commit

---
+++ ms03_template/experiments.py
@@ -0,0 +1,49 @@
+import argparse
+
+from lightning.pytorch import Trainer
+
+from base import BaseModel
+
+
+parser = argparse.ArgumentParser(prog='APP4RS', description='Run Experiments.')
+
+parser.add_argument('--task', type=str, default='slc')
+parser.add_argument('--logging_dir', type=str)
+
+parser.add_argument('--dataset', type=str)
+parser.add_argument('--lmdb_path', type=str)
+parser.add_argument('--metadata_parquet_path', type=str)
+
+parser.add_argument('--num_channels', type=int)
+parser.add_argument('--num_classes', type=int)
+parser.add_argument('--num_workers', type=int)
+parser.add_argument('--batch_size', type=int)
+
+parser.add_argument('--arch_name', type=str)
+parser.add_argument('--pretrained', action='store_true')
+parser.add_argument('--dropout', action='store_true')
+parser.add_argument('--epochs', type=int)
+parser.add_argument('--learning_rate', type=float)
+parser.add_argument('--weight_decay', type=float)
+
+
+def experiments():
+    args = parser.parse_args()
+
+    network = None
+    datamodule = None
+    model = BaseModel(args, datamodule, network)
+
+    trainer = Trainer(
+        accelerator='gpu',
+        devices=[0],
+        enable_checkpointing=True,
+        max_epochs=args.epochs,
+    )
+
+    # training
+    trainer.fit(model)
+
+
+if __name__ == "__main__":
+    experiments()
---
+++ ms03_template/main.py
@@ -0,0 +1,40 @@
+import subprocess
+
+
+def run_training():
+    # Define the command to run your training script with arguments
+    command = [
+        "python", "experiments.py",  # replace with your script's filename
+        "--task", "?",
+        "--logging_dir", "/path/to/logging_dir",
+        "--dataset", "dataset",
+        "--lmdb_path", "/path/to/lmdb",
+        "--metadata_parquet_path", "/path/to/metadata",
+        "--num_channels", "33",
+        "--num_classes", "1000",
+        "--num_workers", "0",
+        "--batch_size", "1",
+        "--arch_name", "abcdefg",
+        "--epochs", "1",
+        "--learning_rate", "0.0",
+        "--weight_decay", "0.0"
+    ]
+
+    # Run the command synchronously and capture output
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    # Check if the process completed successfully
+    if result.returncode == 0:
+        print("Training completed successfully.")
+        print(result.stdout)
+        print("Error:")
+        print(result.stdout)
+    else:
+        print("Training output:")
+        print(result.stdout)
+        print("Error occurred during training:")
+        print(result.stderr)
+
+
+if __name__ == "__main__":
+    run_training()
