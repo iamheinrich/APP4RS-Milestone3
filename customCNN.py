478cd13b6714f213a6f2e427a5896c30d94e04ab
unirepos1 <shokry@campus.tu-berlin.de>
2025-01-19 01:17:47 +0100

    old multispectralTimmModelWrapper version

--- customCNN.py
+++ customCNN.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 
-class CustomCNN(nn.Module):
+import timm
 
+class CustomCNN(nn.Module):
     def __init__(self, num_classes, num_channels):
         super(CustomCNN, self).__init__()
 
@@ -38,3 +39,48 @@ class CustomCNN(nn.Module):
         logits = self.fc(x)
         
         return logits
+    
+class multispectralTimmModelWrapper(nn.Module):
+    def __init__(self, arch_name, num_channels, num_classes, pretrained):
+        super(multispectralTimmModelWrapper, self).__init__()
+
+        #self.arch_name=arch_name
+        #self.num_channels=num_channels
+        #self.num_classes=num_classes
+        #self.pretrained=pretrained
+
+        if arch_name == "ResNet18":
+            #https://huggingface.co/timm/resnet18.a1_in1k
+            self.model = timm.create_model("resnet18.a1_in1k", pretrained=pretrained, in_chans=num_channels, num_classes=num_classes)
+            
+        if arch_name == "ConvNeXt-Nano":
+            #https://huggingface.co/timm/convnext_nano.in12k
+            self.model = timm.create_model("convnext_nano.in12k", pretrained=pretrained, in_chans=num_channels, num_classes=num_classes)
+
+        if arch_name == "ViT-Tiny":
+            #https://huggingface.co/timm/vit_tiny_patch16_224.augreg_in21k
+            self.model = timm.create_model("vit_tiny_patch16_224.augreg_in21k", pretrained=pretrained, in_chans=num_channels, img_size=112, patch_size=8, num_classes=num_classes)
+
+        #data_config = timm.data.resolve_model_data_config(model)                    #TODO check if necessary
+        #transforms = timm.data.create_transform(**data_config, is_training=False)
+
+    def forward(self,x):
+        return self.model(x)
+
+
+def get_network(arch_name, num_channels, num_classes, pretrained):
+    if arch_name == "CustomCNN":
+        model = CustomCNN(num_channels, num_classes)
+    else:
+        model = multispectralTimmModelWrapper(arch_name, num_channels, num_classes, pretrained)
+    
+    #model = model.eval() not necessary since lightning sets mode automatically
+    return model
+
+
+if __name__ == "__main__":
+    for arch_name in ["ResNet18","ConvNeXt-Nano","ViT-Tiny"]:
+        network = get_network(arch_name=arch_name,num_channels=10,num_classes=19,pretrained=True)
+        print(arch_name, dict(network.model.named_children()).keys())
+        print(list(network.model.named_children())[0])
+        print(list(network.model.named_children())[-1],"\n\n")
