cb3fc9b8ab512a985e91a4ebecc91a4d0e0b9650
unirepos1 <shokry@campus.tu-berlin.de>
2025-01-30 06:43:11 +0100

    added dropout and drop_rate to models.py

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import timm
 
 class CustomCNN(nn.Module):
-    def __init__(self, num_channels, num_classes):
+    def __init__(self, num_channels, num_classes, drop_rate):
         super(CustomCNN, self).__init__()
 
         self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=7,padding=3,stride=2)
@@ -16,6 +16,7 @@ class CustomCNN(nn.Module):
         self.bn3 = nn.BatchNorm2d(512)
 
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_rate)
 
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc= nn.Linear(512, num_classes)
@@ -24,34 +25,38 @@ class CustomCNN(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
+        x = self.dropout(x)
         
         x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu(x)
+        x = self.dropout(x)
 
         x = self.conv3(x)
         x = self.bn3(x)
         x = self.relu(x)
+        x = self.dropout(x)
 
         x = self.avg_pool(x)
         x = torch.flatten(x, start_dim=1)
+        x = self.dropout(x)
 
         logits = self.fc(x)
         
         return logits
     
-def get_network(arch_name, num_channels, num_classes, pretrained):
+def get_network(arch_name, num_channels, num_classes, pretrained, drop_rate):
     if arch_name == "CustomCNN":
-        model = CustomCNN(num_channels=num_channels, num_classes=num_classes)
+        model = CustomCNN(num_channels=num_channels, num_classes=num_classes, drop_rate=drop_rate)
     elif arch_name == "ResNet18":
         #https://huggingface.co/timm/resnet18.a1_in1k
-        model = timm.create_model("resnet18.a1_in1k", pretrained=pretrained, in_chans=num_channels, num_classes=num_classes)
+        model = timm.create_model("resnet18.a1_in1k", pretrained=pretrained, in_chans=num_channels, num_classes=num_classes, drop_rate=drop_rate)
     elif arch_name == "ConvNeXt-Nano":
         #https://huggingface.co/timm/convnext_nano.in12k
-        model = timm.create_model("convnext_nano.in12k", pretrained=pretrained, in_chans=num_channels, num_classes=num_classes)
+        model = timm.create_model("convnext_nano.in12k", pretrained=pretrained, in_chans=num_channels, num_classes=num_classes, drop_rate=drop_rate)
     elif arch_name == "ViT-Tiny":
         #https://huggingface.co/timm/vit_tiny_patch16_224.augreg_in21k
-        model = timm.create_model("vit_tiny_patch16_224.augreg_in21k", pretrained=pretrained, in_chans=num_channels, img_size=112, patch_size=8, num_classes=num_classes)
+        model = timm.create_model("vit_tiny_patch16_224.augreg_in21k", pretrained=pretrained, in_chans=num_channels, img_size=112, patch_size=8, num_classes=num_classes, drop_rate=drop_rate)
     else:
         raise NotImplementedError(f"arch_name=={arch_name} is not handled!!")
 
@@ -62,7 +67,7 @@ def get_network(arch_name, num_channels, num_classes, pretrained):
 
 if __name__ == "__main__":
     for arch_name in ["ResNet18","ConvNeXt-Nano","ViT-Tiny"]:
-        network = get_network(arch_name=arch_name,num_channels=10,num_classes=19,pretrained=True)
+        network = get_network(arch_name=arch_name,num_channels=10,num_classes=19,pretrained=True, drop_rate=0.3)
         print(arch_name, dict(network.named_children()).keys())
         print(list(network.named_children())[0])
         print(list(network.named_children())[-1],"\n\n")
